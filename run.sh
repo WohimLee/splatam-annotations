fc33e30ac872ab2e82156c8cd3245745e8e7211d
AZen <418130444@qq.com>
2024-05-06 17:40:07 +0800

    update

---
+++ run.sh
@@ -0,0 +1,5 @@
+# To run SplaTAM on the room0 scene, run the following command:
+python scripts/splatam.py configs/replica/splatam.py
+
+# # To run SplaTAM-S on the room0 scene, run the following command:
+# python scripts/splatam.py configs/replica/splatam_s.py
